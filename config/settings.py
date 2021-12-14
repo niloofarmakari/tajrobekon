568d85b1380839c427aff532066999b1d9ab50d6
Danial Keimasi <danialkeimasi@gmail.com>
2021-12-14 12:27:54 +0330

    Downgrade django to 3.2 and add new auth_user_model

--- config/settings.py
+++ config/settings.py
@@ -121,6 +121,8 @@ class Common(Configuration):
 
     DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+    AUTH_USER_MODEL = "authentication.User"
+
     LOGIN_REDIRECT_URL = "/"
     LOGIN_REDIRECT_URL = "/"
 
